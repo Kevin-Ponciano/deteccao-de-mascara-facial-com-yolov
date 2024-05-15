89263e82d475bb9366bb52f7390296d04f063434
Kevin <kevin.ponciano@outlook.com>
2024-05-15 15:03:44 -0300

    prepare data

---
+++ get_coco.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Função para encontrar arquivos .jpg e escrever em um arquivo
+find_jpg_files() {
+    local directory=$1
+    local output_file=$2
+
+    # Mudar para o diretório
+    cd "$directory"
+
+    # Encontrar todos os arquivos .jpg no diretório atual e subdiretórios
+    # e escrever os caminhos no arquivo de saída
+    find $(pwd) -type f -name "*.jpg" > "$output_file"
+}
+
+# Armazenar o caminho atual
+CURRENT_PATH=$(pwd)
+
+# Chamadas da função para diferentes diretórios e arquivos de saída
+find_jpg_files "$CURRENT_PATH/data/teste" "teste.txt"
+find_jpg_files "$CURRENT_PATH/data/treino" "treino.txt"
+find_jpg_files "$CURRENT_PATH/data/validacao" "validacao.txt"
